714c9bef8e9f1de0653340ba0eecd319979a89b7
Dylan Moore <dylankmoore@gmail.com>
2024-04-23 20:14:42 -0500

    created data for genres & playlists

--- Data/PlaylistData.cs
+++ Data/PlaylistData.cs
@@ -6,6 +6,33 @@ namespace BE_MusicStreaming.Data
     {
         public static List<Playlist> Playlists = new List<Playlist>
         {
+            new Playlist
+            {
+                Id = 1,
+                Name = "Y2K Alt Anthems",
+                DateCreated = new DateTime(2024, 4, 01),
+                Public = true,
+                IsFavorite = false,
+                UserId = 1
+            },
+            new Playlist
+            {
+                Id = 2,
+                Name = "Pop Princesses",
+                DateCreated = new DateTime(2024, 3, 15),
+                Public = true,
+                IsFavorite = true,
+                UserId = 1
+            },
+            new Playlist
+            {
+                Id = 3,
+                Name = "Love & Heartbreak",
+                DateCreated = new DateTime(2024, 4, 20),
+                Public = false,
+                IsFavorite = true,
+                UserId = 1
+            }
         };
     }
 }
